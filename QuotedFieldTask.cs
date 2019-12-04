17fc54ee8a84577cdbfe205d7d58ec2a913cf676
Антон Забабурин <asumosan@gmail.com>
2019-12-04 07:07:44 +0500

    1st job done

--- QuotedFieldTask.cs
+++ QuotedFieldTask.cs
@@ -12,12 +12,14 @@ namespace TableParser
     {
         [TestCase("''", 0, "", 2)]
         [TestCase("'a'", 0, "a", 3)]
+        [TestCase("'a\''", 0, "a'", 5)]
+
         public void Test(string line, int startIndex, string expectedValue, int expectedLength)
         {
             var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
             Assert.AreEqual(actualToken, new Token(expectedValue, startIndex, expectedLength));
         }
-
+        
         // Добавьте свои тесты
     }
 
@@ -25,22 +27,40 @@ namespace TableParser
     {
         public static Token ReadQuotedField(string line, int startIndex)
         {
-                char[] startChar = {'\'','"'};                  //Записываем какие кавычки " или ';
-                string actualline = line.Substring(startIndex);     //Берём подстроку за кавычками;
-                int tokenLenght = actualline.Length;                
-                for (int i = 0; i < line.Length; i++)
-                {
-                
+            int tokenLenght=1;
+            int quotedValue=1;
+            var startChar = line[startIndex];
+            string tokenValue = null;
 
-                if (startChar.Contains(line[i]) && (line[i] =='\'' || line[i] == '\"' ) && (i - 1 > 0) && (actualline[i - 1] != '\\'))
+            if (startIndex+1<line.Length && line[startIndex + 1] == startChar)
+                return new Token(line, startIndex, tokenLenght+1);
+            
+            else if (startChar != '"' && startChar != '\'')
+            {
+                tokenValue=line.Substring(startIndex).Split(' ')[0];
+                tokenLenght = tokenValue.Length;
+            }
+           
+            else
+                for (int i = startIndex + 1; i < line.Length; i++)
+                {
+                                                              
+                    if (line[i] == startChar && line[i - 1] != '\\')
                     {
-                        actualline = line.Substring(startIndex + 1, i-1);
-                        tokenLenght = actualline.Length + 2;
-                        ReadQuotedField(actualline,i);
+                        quotedValue += 1;
+                        tokenLenght = tokenValue.Length + quotedValue;  
                         break;
                     }
+                    else if (line[i] == '\\' && i + 1 < line.Length&&line[i+1]==startChar)
+                        quotedValue++;                      
+                    else
+                    tokenValue += line[i];
+                    tokenLenght = tokenValue.Length + quotedValue;      
                 }
-            return new Token(actualline, startIndex, tokenLenght);
+            
+            if (startIndex + tokenLenght + 1 < line.Length)
+                ReadQuotedField(line, startIndex + tokenLenght + 1);
+            return new Token(tokenValue, startIndex, tokenLenght);
         }
     }
 }
